362304853582b426af97254f2cffb36e8aba88e5
git <prveen@abc.com>
2017-05-11 15:04:18 -0400

    Edit script

--- provision_script.tf
+++ provision_script.tf
@@ -17,5 +17,5 @@ resource "azurerm_virtual_network" "helloterraformnetwork" {
 	name = "acctvn"
 	address_space = ["10.0.0.0/16"]
 	location = "West US"
-	resource_group_name = "azurerm_resource_group.helloterraform.terraformtest"
+	resource_group_name = "${azurerm_resource_group.helloterraform.name}"
 }
