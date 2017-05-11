ea4348131c2669dc581a20a86d65a49d39d52a6f
git <prveen@abc.com>
2017-05-11 12:35:44 -0400

    first commit

---
+++ provision_script.tf
@@ -0,0 +1,13 @@
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  subscription_id = "fd19c99e-99f0-4111-bb5f-9f3c5e8cac24"
+  client_id       = "f8cc7b14-bddd-4b87-9630-0d8816bcf77e"
+  client_secret   = "praveen"
+  tenant_id       = "5f1dc286-8320-4bd2-a524-e68237d755ee"
+}
+
+# create a resource group 
+resource "azurerm_resource_group" "helloterraform" {
+    name = "terraformtest"
+    location = "West US"
+}
