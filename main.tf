b0825c1fbce4236746237f82824db7fbca2c2c33
pxa14760 <praveenreddyarumalla@gmail.com>
2020-07-13 14:55:41 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,16 @@
 provider "aws" {
 	region = "us-east-1"
 }
-#
-#module "sns_topic" {
-#  source 	  = "./sns"
-#  name      = "sample_sns_topic"
-#}
-#
-#module "sqs" {
-#  source      = "./sqs"
-#  name        = "sample_sqs"
-#}
-#
 
-
-resource "aws_s3_bucket" "terraform_state" {
-	bucket = "parumalla-bucket-state"
-
-	versioning {
-		enabled = true
-	}
-
-	lifecycle {
-		prevent_destroy = true
-	}
+module "sns_topic" {
+  source 	  = "./sns"
+  name      = "sample_sns_topic"
 }
+
+module "sqs" {
+  source      = "./sqs"
+  name        = "sample_sqs"
+}
+
+
+
