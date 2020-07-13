72fc62467aa87c79d9a344f753860e7b2c734950
Praveen Reddy Arumalla <parumalla@dxc.com>
2020-07-13 13:13:03 -0500

    bucket name correction

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 
 
 resource "aws_s3_bucket" "terraform_state" {
-	bucket = "terraform-up-and-running-state"
+	bucket = "parumalla-bucket-state"
 
 	versioning {
 		enabled = true
