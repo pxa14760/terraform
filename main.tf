4eeae739ed7001c65ba2cd575a475cf890f90f8b
Praveen Reddy Arumalla <parumalla@dxc.com>
2020-07-13 13:06:30 -0500

    create bucket

--- main.tf
+++ main.tf
@@ -1,13 +1,27 @@
 provider "aws" {
 	region = "us-east-1"
 }
+#
+#module "sns_topic" {
+#  source 	  = "./sns"
+#  name      = "sample_sns_topic"
+#}
+#
+#module "sqs" {
+#  source      = "./sqs"
+#  name        = "sample_sqs"
+#}
+#
 
-module "sns_topic" {
-  source 	  = "./sns"
-  name      = "sample_sns_topic"
-}
 
-module "sqs" {
-  source      = "./sqs"
-  name        = "sample_sqs"
+resource "aws_s3_bucket" "terraform_state" {
+	bucket = "terraform-up-and-running-state"
+
+	versioning {
+		enables = true
+	}
+
+	lifecycle {
+		prevent_destry = true
+	}
 }
