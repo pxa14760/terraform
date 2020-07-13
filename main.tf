de53d032fb2b03739fd271aa2112c60634e32ab0
Praveen Reddy Arumalla <parumalla@dxc.com>
2020-07-12 22:17:23 -0500

    sqs

--- main.tf
+++ main.tf
@@ -2,9 +2,7 @@ provider "aws" {
 	region = "us-east-1"
 }
 
-resource "aws_instance" "terraform_test" {
-	ami = "ami-40d28157"
-	instance_type = "t2.micro"
 
-	
+data "aws_sqs_queue" "terraform_sqs" {
+  name = "sqs_using_tf"
 }
