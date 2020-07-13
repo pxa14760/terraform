6008ee11600065714fddcdaa8ba0be3964a16728
Praveen Reddy Arumalla <parumalla@dxc.com>
2020-07-13 18:41:41 -0500

    syntax issues

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "parumalla-bucket-state"
     key            = "global/s3/terraform.tfstate"
-    region         = “us-east-1"
+    region         = "us-east-1"
     
   }
 }
