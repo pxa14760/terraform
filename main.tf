a0fb717366c428c76eca3d06b2539c1724663187
Praveen Reddy Arumalla <parumalla@dxc.com>
2020-07-12 23:44:54 -0500

    add sqs

--- main.tf
+++ main.tf
@@ -2,7 +2,9 @@ provider "aws" {
 	region = "us-east-1"
 }
 
-
-data "aws_sqs_queue" "terraform_sqs" {
-  name = "sqs_using_tf"
+module "sqs" {
+  source      = "./sqs"
+  environment = "staging"
+  project     = "example"
+  name        = ["sample_sqs"]
 }
