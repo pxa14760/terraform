7d8bfaf780622bdf160a54ea38a27c1f42912aaf
Praveen Reddy Arumalla <parumalla@dxc.com>
2020-07-13 00:01:37 -0500

    deleted dead letter queue

--- sqs/main.tf
+++ sqs/main.tf
@@ -1,27 +1,28 @@
-#main.tf
-data "template_file" "redrive_policy" {
-  template = <<EOF
-{
-  "deadLetterTargetArn":"$${dlq}",
-  "maxReceiveCount":$${mrc}
+resource "aws_sqs_queue" "this" {
+  count = var.create ? 1 : 0
+
+  name        = var.name
+  name_prefix = var.name_prefix
+
+  visibility_timeout_seconds  = var.visibility_timeout_seconds
+  message_retention_seconds   = var.message_retention_seconds
+  max_message_size            = var.max_message_size
+  delay_seconds               = var.delay_seconds
+  receive_wait_time_seconds   = var.receive_wait_time_seconds
+  policy                      = var.policy
+  redrive_policy              = var.redrive_policy
+  fifo_queue                  = var.fifo_queue
+  content_based_deduplication = var.content_based_deduplication
+
+  kms_master_key_id                 = var.kms_master_key_id
+  kms_data_key_reuse_period_seconds = var.kms_data_key_reuse_period_seconds
+
+  tags = var.tags
 }
-EOF
 
+data "aws_arn" "this" {
+  count = var.create ? 1 : 0
 
-  vars = {
-    dlq = var.dead_letter_queue
-    mrc = var.max_receive_count
-  }
-}
+  arn = aws_sqs_queue.this[0].arn
 
-resource "aws_sqs_queue" "queue" {
-  count = length(var.name)
-  name = "${var.environment}_${var.project}_${var.name[count.index]}${var.fifo_queue == "true" ? ".fifo" : ""}"
-  visibility_timeout_seconds = var.visibility_timeout_seconds
-  delay_seconds = var.delay_seconds
-  max_message_size = var.max_message_size # 256 KB
-  message_retention_seconds = var.message_retention_seconds # 4 days
-  receive_wait_time_seconds = var.receive_wait_time_seconds
-  redrive_policy = length(var.dead_letter_queue) > 0 ? data.template_file.redrive_policy.rendered : ""
-  fifo_queue = var.fifo_queue
 }
