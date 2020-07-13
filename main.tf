606e3240e47cd75f60d173f257ba1987d2e0d6ac
Praveen Reddy Arumalla <parumalla@dxc.com>
2020-07-13 00:37:19 -0500

    add sns topic

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ provider "aws" {
 	region = "us-east-1"
 }
 
+module "sns_topic" {
+  source 	  = "./sns"
+  name      = "sample_sns_topic"
+}
+
 module "sqs" {
   source      = "./sqs"
   name        = "sample_sqs"
