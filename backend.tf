cd6068548265e8b5a5d6f7e6818d6259fd874b32
pxa14760 <praveenreddyarumalla@gmail.com>
2020-07-13 23:20:22 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "parumalla-bucket-state"
     key            = "global/s3/terraform.tfstate"
     region         = "us-east-1"
-    
+    dynamodb_table = "terraform-lock"
   }
 }
 
